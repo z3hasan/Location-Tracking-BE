6cf1424f7cce310a7eac176621c86bc7b9571bd4
Zara Hasan <z3hasan@outlook.com>
2020-05-14 20:16:41 -0400

    port 5000 or environment port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+}).listen(PORT);
