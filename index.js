9ff9bc103672a789b0b015ea3acb0f86bea323f7
Zara Hasan <z3hasan@outlook.com>
2020-05-15 04:12:27 -0400

    including express, ejs

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+//importing modules
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
-const PORT = process.env.PORT || 5000;
+const port = normalizePort(process.env.PORT || '5000');
 
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demo.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(PORT);
+const app = express();
+
+app.set('port',port);
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',(req, res) => res.render('demo'));
+app.listen(port, () => console.log(`Listening on ${ port }`));
+
+//http.createServer(function (req, res) {
+  //fs.readFile('demo.html', function(err, data) {
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.write(data);
+    //return res.end();
+  //});
+//}).listen(port);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
