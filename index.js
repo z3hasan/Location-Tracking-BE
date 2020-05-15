7bd14dee88b8164f208958a4b7d2c56070fee21c
Zara Hasan <z3hasan@outlook.com>
2020-05-14 20:46:00 -0400

    geolocation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var http = require('http');
+
 const PORT = process.env.PORT || 5000;
 
+var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! '+ PORT);
+  fs.readFile('demo.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(PORT);
