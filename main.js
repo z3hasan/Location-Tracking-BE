83f0c69ca7654f673e6ae2931540501454e62440
Zara Hasan <z3hasan@outlook.com>
2020-05-17 02:33:40 -0400

    POST: geolocation

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-
-const port = normalizePort(process.env.PORT || '5000');
+var bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 
+//Setting port
+const port = normalizePort(process.env.PORT || '5000');
+
+//setting express app
 const app = express();
 
 app.set('port',port);
@@ -16,19 +19,13 @@ app.set('port',port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.text());
+//app.use(bodyParser.json());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.listen(port, () => console.log(`Listening on ${ port }`));
-
-//http.createServer(function (req, res) {
-  //fs.readFile('demo.html', function(err, data) {
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.write(data);
-    //return res.end();
-  //});
-//}).listen(port);
-
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('demo');
+  console.log("testing");
+});
+
+router.post('/test',bodyParser.text(), function(req, res, next) {
+  console.log(req.body);
+});
+
+router.post('/error',bodyParser.text(), function(req, res, next) {
+  console.log(req.body);
 });
 
 module.exports = router;
